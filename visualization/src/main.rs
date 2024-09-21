6b9b28527ccda13cb053c7ca0e9587a18aaf8c08
uowis <mail@example.com>
2024-09-21 17:04:18 +0200

    add polars and query actual row data from stops_clustered.csv

--- visualization/src/main.rs
+++ visualization/src/main.rs
@@ -1,26 +1,57 @@
 use actix_cors::Cors;
 use actix_files::Files;
-use actix_web::{get, web, App, Result, HttpServer, Responder};
+use actix_web::error::ErrorInternalServerError;
+use actix_web::{get, web, App, HttpServer, Responder, Result};
+use polars::frame::UniqueKeepStrategy;
+use polars::prelude::{col, AnyValue, LazyCsvReader, LazyFileListReader, PolarsError};
 use serde::Serialize;
 
 #[derive(Serialize)]
 struct Stats {
-    num_stops: usize,
-    num_clusters: usize,
-    num_routes: usize,
-    num_trips: usize,
+    num_stops: u32,
+    num_clusters: u32,
 }
 
 #[get("/api/v1/stats")]
 async fn stats() -> Result<impl Responder> {
-    let stats = Stats {
-        num_stops: 42,
-        num_clusters: 2,
-        num_routes: 420,
-        num_trips: 420000,
-    };
+    fn collect_stats() -> Result<Stats, PolarsError> {
+        let clustered_stops = LazyCsvReader::new("../data/tmp/stp/stops_clustered.csv").finish()?;
+        
+        let counts = clustered_stops.clone().count().collect()?;
+        let num_stops = match counts.get_columns().get(0).unwrap().get(0)? {
+            AnyValue::UInt32(count) => count,
+            _ => { return Err(PolarsError::ComputeError("Failed to calculate num_stops".into())) }
+        };
+        
+        let cluster_count = clustered_stops.clone()
+            .select([col("cluster_id")])
+            .unique(None, UniqueKeepStrategy::Any)
+            .count().collect()?;
+        let num_clusters = match cluster_count.get_columns().get(0).unwrap().get(0)? {
+            AnyValue::UInt32(count) => count,
+            _ => { return Err(PolarsError::ComputeError("Failed to calculate num_clusters".into())) }
+        };
+
+        Ok(Stats {
+            num_stops,
+            num_clusters,
+        })
+    }
     
-    Ok(web::Json(stats))
+    match collect_stats() {
+        Ok(stats) => {
+            Ok(web::Json(stats))
+        },
+        Err(err) => {
+            match err {
+                PolarsError::IO { .. } => {
+                    // TODO: Use a better way to determine whether service is ready
+                    Err(actix_web::Error::from(ErrorInternalServerError("Unable to read stops_clustered.csv")))
+                },
+                _ => Err(actix_web::Error::from(ErrorInternalServerError("")))
+            }
+        }
+    }
 }
 
 #[actix_web::main]
