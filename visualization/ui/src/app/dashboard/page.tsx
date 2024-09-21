5913dde9a3032d5e8aa29aae3e54b8c51904cdcb
uowis <mail@example.com>
2024-09-21 13:03:33 +0200

    add more content to home page

--- visualization/ui/src/app/dashboard/page.tsx
+++ visualization/ui/src/app/dashboard/page.tsx
@@ -1,6 +1,31 @@
 import {CardStats} from "@/components/ui/dashboard/card-stats";
 
-import { MapPin, Waypoints, Navigation } from "lucide-react";
+import { MapPin, Waypoints, Navigation, ArrowUpRight } from "lucide-react";
+import {Card, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+
+import Link from "next/link";
+
+const STEPS = [
+    {
+        title: "Harvesting data",
+        cards: []
+    },
+    {
+        title: "Importing data",
+        cards: []
+    },
+    {
+        title: "Clustering",
+        cards: [
+            { title: "Clustered stops", description: "Explore the calculated clustering", link: "/dashboard/map" }
+        ]
+    },
+    {
+        title: "Pre-Calculate connections",
+        cards: []
+    }
+]
 
 export default function HomePage() {
     return (
@@ -10,11 +35,32 @@ export default function HomePage() {
                     title="Stops" subtitle="Stop places accross datasets" value="10,002"
                     icon={<MapPin />} />
                 <CardStats
-                    title="Routes" subtitle="Routes accross datasets" value="4,202"
+                    title="Lines" subtitle="Lines accross datasets" value="4,202"
                     icon={<Waypoints />} />
                 <CardStats
                     title="Trips" subtitle="Trips places accross datasets" value="34,502"
                     icon={<Navigation />} />
+            </div>
+            <div className="flex flex-col gap-4 md:gap-8">
+                {STEPS.map((step) => (
+                    <>
+                        <h2 className="text-lg font-semibold leading-none tracking-tight">{step.title}</h2>
+                        {step.cards.map((card) => (
+                            <Link href={card.link} key={card.link}>
+                                <Card className="flex flex-row items-center">
+                                    <CardHeader className="bg-muted/50">
+                                        <CardTitle>{card.title}</CardTitle>
+                                        <CardDescription>{card.description}</CardDescription>
+                                        <Button variant="link" className="gap-1">
+                                            Learn more
+                                            <ArrowUpRight className="w-4 h-4" />
+                                        </Button>
+                                    </CardHeader>
+                                </Card>
+                            </Link>
+                        ))}
+                    </>
+                ))}
             </div>
         </div>
     );
