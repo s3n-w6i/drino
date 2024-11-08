e91695af8568204c15a69d5803c09343c4ec4ebd
uowis <mail@example.com>
2024-11-08 11:53:11 +0100

    change stopid formatting

--- common/src/types/mod.rs
+++ common/src/types/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug, Display, Formatter};
+use chrono::{DateTime, NaiveDate, Utc};
 use polars::datatypes::AnyValue;
 
 pub mod dataset;
@@ -5,8 +7,20 @@ pub mod dataset;
 // a continuous stop id
 // "continuous" means that if we have n stops, all ids are from 0,...,n-1 and no number in that range
 // is unused
-#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd)]
 pub struct StopId(pub u32);
+
+impl Display for StopId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("s:{}", self.0))
+    }
+}
+
+impl Debug for StopId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("s:{}", self.0))
+    }
+}
 
 impl <'a> From<StopId> for AnyValue<'a> {
     fn from(value: StopId) -> AnyValue<'a> {
