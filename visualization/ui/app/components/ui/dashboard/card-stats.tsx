58e3a1acfd6979e3b21d400030d36b30d8e9e300
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2024-12-20 22:22:17 +0100

    Fetch stats from API

--- visualization/ui/app/components/ui/dashboard/card-stats.tsx
+++ visualization/ui/app/components/ui/dashboard/card-stats.tsx
@@ -8,16 +8,18 @@ import {
     CardTitle,
 } from "~/components/ui/card"
 import {cn} from "~/lib/utils";
+import {Skeleton} from "~/components/ui/skeleton";
 
 export interface CardStatsProps extends React.HTMLAttributes<HTMLDivElement> {
     title: string,
     subtitle: string,
-    value?: string,
+    value?: string | undefined,
+    valueLoading?: boolean,
     icon: React.ReactElement
 }
 
 const CardStats = React.forwardRef<HTMLDivElement, CardStatsProps>(
-    ({ className, title, subtitle, value, icon, ...props }, ref) => (
+    ({ className, title, subtitle, value, valueLoading, icon, ...props }, ref) => (
         <Card ref={ref}
               className={className}
               {...props}>
@@ -26,7 +28,8 @@ const CardStats = React.forwardRef<HTMLDivElement, CardStatsProps>(
                 {React.cloneElement(icon, { className: cn("h-5 w-5 text-muted-foreground", className) })}
             </CardHeader>
             <CardContent>
-                <div className="text-2xl font-bold">{value}</div>
+                {!valueLoading && <div className="text-2xl font-bold">{value}</div>}
+                {valueLoading && <Skeleton className="max-w-full w-32 h-6 my-1" />}
                 {subtitle && <p className="text-xs text-muted-foreground">{subtitle}</p>}
             </CardContent>
         </Card>
--- visualization/ui/app/routes/home.tsx
+++ visualization/ui/app/routes/home.tsx
@@ -1,13 +1,14 @@
-import type { Route } from "./+types/home";
+import type {Route} from "./+types/home";
 import {ArrowUpRight, MapPin, Navigation, Waypoints} from "lucide-react";
 import {CardStats} from "~/components/ui/dashboard/card-stats";
 import {NavLink} from "react-router";
 import {Card, CardDescription, CardHeader, CardTitle} from "~/components/ui/card";
 import {Button} from "~/components/ui/button";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
     return [
-        { title: "Home" },
+        {title: "Home"},
     ];
 }
 
@@ -23,7 +24,7 @@ const STEPS = [
     {
         title: "Clustering",
         cards: [
-            { title: "Clustered stops", description: "Explore the calculated clustering", link: "map" }
+            {title: "Clustered stops", description: "Explore the calculated clustering", link: "map"}
         ]
     },
     {
@@ -32,19 +33,40 @@ const STEPS = [
     }
 ]
 
+interface Stats {
+    num_stops: number
+}
+
 export default function Home() {
+    let [stats, setStats] = useState<Stats | undefined>(undefined);
+    let [statsLoading, setStatsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        fetch("https://localhost:3001/api/v1/stats")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response;
+            })
+            .then(data => setStats(data))
+            .finally(() => setStatsLoading(false));
+    }, []);
+
     return (
         <div className="grid items-_start gap-4 p-4 sm:p-6 md:gap-8">
             <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
                 <CardStats
-                    title="Stops" subtitle="Stop places accross datasets" value="10,002"
-                    icon={<MapPin />} />
+                    title="Stops" subtitle="Stop places accross datasets"
+                    valueLoading={statsLoading}
+                    value={stats?.num_stops?.toString()}
+                    icon={<MapPin/>}/>
                 <CardStats
                     title="Lines" subtitle="Lines accross datasets" value="4,202"
-                    icon={<Waypoints />} />
+                    icon={<Waypoints/>}/>
                 <CardStats
                     title="Trips" subtitle="Trips places accross datasets" value="34,502"
-                    icon={<Navigation />} />
+                    icon={<Navigation/>}/>
             </div>
             <div className="flex flex-col gap-4 md:gap-8">
                 {STEPS.map((step) => (
@@ -58,7 +80,7 @@ export default function Home() {
                                         <CardDescription>{card.description}</CardDescription>
                                         <Button variant="link" className="gap-1">
                                             Learn more
-                                            <ArrowUpRight className="w-4 h-4" />
+                                            <ArrowUpRight className="w-4 h-4"/>
                                         </Button>
                                     </CardHeader>
                                 </Card>
