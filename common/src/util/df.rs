571a5da102208448270dacdc9243014b1838f067
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2024-12-23 14:38:24 +0100

    add utility for writing out files

--- common/src/util/df.rs
+++ common/src/util/df.rs
@@ -1,6 +1,10 @@
+use std::fs::{create_dir_all, File};
+use std::path::PathBuf;
 use polars::datatypes::AnyValue;
 use polars::error::{PolarsError, PolarsResult};
-use polars::prelude::LazyFrame;
+use polars::frame::DataFrame;
+use polars::io::SerWriter;
+use polars::prelude::{CsvWriter, IpcWriter, LazyFrame, ParquetWriter};
 
 pub fn count(frame: LazyFrame) -> PolarsResult<u32> {
         let count = frame.count()
@@ -9,4 +13,39 @@ pub fn count(frame: LazyFrame) -> PolarsResult<u32> {
             AnyValue::UInt32(count) => Ok(count),
             _ => Err(PolarsError::ComputeError("Count was not u32".into()))
         }
+}
+
+pub enum FileType {
+    CSV,
+    IPC,
+    PARQUET,
+}
+
+pub fn write_file(
+    path: PathBuf,
+    format: FileType,
+    mut df: DataFrame
+) -> Result<(), PolarsError> {
+    if let Some(parent) = path.parent() {
+        create_dir_all(parent)?;
+    }
+
+    let mut file = File::create(path)?;
+
+    match format {
+        FileType::CSV => {
+            CsvWriter::new(&mut file).finish(&mut df)?;
+            Ok(())
+        },
+        FileType::IPC => {
+            IpcWriter::new(&mut file).finish(&mut df)?;
+            Ok(())
+        },
+        FileType::PARQUET => {
+            ParquetWriter::new(&mut file).finish(&mut df)?;
+            Ok::<(), PolarsError>(())
+        },
+    }?;
+
+    Ok(())
 }
