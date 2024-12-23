2341dfea93d042c418437c154eabf90618143325
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2024-12-23 14:48:20 +0100

    remove old write_tmp_file.rs

--- routing/src/lib.rs
+++ routing/src/lib.rs
@@ -4,8 +4,5 @@ pub mod tp;
 pub mod transfers;
 pub mod algorithm;
 mod direct_connections;
-mod tests;
-mod write_tmp_file;
 mod journey;
-
-pub(crate) use write_tmp_file::write_tmp_file as write_tmp_file;
+mod algorithms;
