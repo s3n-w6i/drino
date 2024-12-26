53b76d6c4e9b59beb00f22eb410dfa03708e9ece
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2024-12-26 17:18:08 +0100

    consume status api and display it in the dashboard

--- visualization/ui/app/routes/home.tsx
+++ visualization/ui/app/routes/home.tsx
@@ -1,11 +1,12 @@
 import type {Route} from "./+types/home";
-import {ArrowUpRight, MapPin, Navigation, Waypoints} from "lucide-react";
+import {ArrowUpRight, CheckIcon, MapPin, Navigation, Waypoints, XIcon} from "lucide-react";
 import {CardStats} from "~/components/ui/dashboard/card-stats";
 import {NavLink} from "react-router";
 import {Card, CardDescription, CardHeader, CardTitle} from "~/components/ui/card";
 import {Button} from "~/components/ui/button";
 import {useEffect, useState} from "react";
 import {LoadingSpinner} from "~/components/ui/spinner";
+import {Skeleton} from "~/components/ui/skeleton";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -13,27 +14,63 @@ export function meta({}: Route.MetaArgs) {
     ];
 }
 
-const STEPS = [
+interface Job {
+    title: string;
+    id: string;
+    status: JobStatus;
+    cards: JobCard[];
+}
+
+enum JobStatus {
+    Queued,
+    Running,
+    Succeeded,
+    Failed,
+}
+
+interface JobCard {
+    title: string;
+    description: string;
+    link: string;
+}
+
+const JOBS: Job[] = [
     {
         title: "Harvesting data",
+        id: "HarvestData",
+        status: JobStatus.Queued,
         cards: []
     },
     {
         title: "Importing data",
+        id: "ImportData",
+        status: JobStatus.Queued,
+        cards: []
+    },
+    {
+        title: "Validating data",
+        id: "ValidateData",
+        status: JobStatus.Queued,
         cards: []
     },
     {
         title: "Clustering",
+        id: "PreprocessingClustering",
+        status: JobStatus.Queued,
         cards: [
             {title: "Clustered stops", description: "Explore the calculated clustering", link: "map"}
         ]
     },
     {
         title: "Pre-Calculate connections within clusters",
+        id: "PreprocessingLocalTransferPatterns",
+        status: JobStatus.Queued,
         cards: []
     },
     {
-        title: "Pre-Calculate connections between clusters",
+        title: "Pre-Calculate connections (Long-distance)",
+        id: "PreprocessingLongDistanceTransferPatterns",
+        status: JobStatus.Queued,
         cards: []
     }
 ]
@@ -44,6 +81,8 @@ interface Stats {
 }
 
 export default function Home() {
+    let [jobs, setJobs] = useState<Job[]>(JOBS);
+
     let [stats, setStats] = useState<Stats | undefined>(undefined);
     let [statsLoading, setStatsLoading] = useState<boolean>(true);
 
@@ -57,6 +96,35 @@ export default function Home() {
             })
             .then(data => setStats(data))
             .finally(() => setStatsLoading(false));
+    }, []);
+
+    useEffect(() => {
+        const sse = new EventSource('https://localhost:3001/api/v1/status');
+
+        sse.onerror = () => {
+            console.error("sse error");
+            setJobs([]);
+            sse.close();
+        };
+
+        sse.onmessage = (msg) => {
+            const data = JSON.parse(msg.data);
+
+            for (const [jobId, newStatus] of Object.entries(data)) {
+                const newJobs = jobs.map((j) => {
+                    if (j.id === jobId) {
+                        j.status = JobStatus[newStatus as keyof typeof JobStatus];
+                    }
+                    return j;
+                });
+
+                setJobs(newJobs);
+            }
+        }
+
+        return () => {
+            sse.close();
+        };
     }, []);
 
     return (
@@ -79,12 +147,20 @@ export default function Home() {
                     icon={<Navigation/>}/>
             </div>
             <div className="flex flex-col gap-4 md:gap-8">
-                {STEPS.map((step) => (
-                    <>
-                        <h2 className="text-lg font-semibold leading-none tracking-tight flex items-center gap-3">
-                            <LoadingSpinner size={18} /> {step.title}
+                {JOBS.map((job) => (
+                    <div key={job.id}>
+                        <h2 className="text-lg font-semibold leading-none tracking-tight flex items-center gap-3 h-5">
+                            {job.status === JobStatus.Running && <LoadingSpinner size={20}/>}
+                            {job.status === JobStatus.Queued && <Skeleton className="bg-gray-200 w-5 h-5 rounded-full" />}
+                            {job.status === JobStatus.Succeeded && <div className="bg-green-600 w-5 h-5 rounded-full">
+                                <CheckIcon className="w-3 h-full mx-auto text-green-100" />
+                            </div>}
+                            {job.status === JobStatus.Failed && <div className="bg-destructive w-5 h-5 rounded-full">
+                                <XIcon className="w-3 h-full mx-auto text-red-100" />
+                            </div>}
+                            {job.title}
                         </h2>
-                        {step.cards.map((card) => (
+                        {job.cards.map((card) => (
                             <NavLink to={card.link} key={card.link}>
                                 <Card className="flex flex-row items-center">
                                     <CardHeader className="bg-muted/50">
@@ -98,7 +174,7 @@ export default function Home() {
                                 </Card>
                             </NavLink>
                         ))}
-                    </>
+                    </div>
                 ))}
             </div>
         </div>
