8670bfddd90df22385de216afe24edc659fe41d4
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2024-12-27 14:15:47 +0100

    consume config api and display it in the dashboard

--- visualization/ui/app/routes/datasets.tsx
+++ visualization/ui/app/routes/datasets.tsx
@@ -3,14 +3,46 @@ import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "~/c
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "~/components/ui/card";
 import {Badge} from "~/components/ui/badge";
 import type {Route} from "../../.react-router/types/app/routes/+types/home";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
     return [
-        { title: "Datasets" },
+        {title: "Datasets"},
     ];
 }
 
+interface Config {
+    datasets: Dataset[],
+    dataset_groups: any[]
+}
+
+interface Dataset {
+    id: string;
+    format: string;
+    license: string;
+    groups: string[];
+    src: any;
+}
+
 export default function DatasetsPage() {
+    let [configLoaded, setConfigLoaded] = useState(false);
+    let [datasets, setDatasets] = useState<Dataset[]>([]);
+
+    useEffect(() => {
+        fetch("https://localhost:3001/api/v1/config")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+
+                throw response;
+            })
+            .then(data => {
+                setDatasets(data.datasets);
+            })
+            .finally(() => setConfigLoaded(true));
+    }, []);
+
     return (
         <>
             <div className="grid flex-1 items-_start gap-4 p-4 sm:p-6 md:gap-8">
@@ -29,17 +61,27 @@ export default function DatasetsPage() {
                                     <TableHead>
                                         Format
                                     </TableHead>
+                                    <TableHead>
+                                        License
+                                    </TableHead>
                                 </TableRow>
                             </TableHeader>
                             <TableBody>
-                                <TableRow onClick={() => { alert("clickidoo") }}>
-                                    <TableCell>
-                                        de:vvs:gtfs
-                                    </TableCell>
-                                    <TableCell>
-                                        <Badge variant="secondary">GTFS</Badge>
-                                    </TableCell>
-                                </TableRow>
+                                {datasets.map(dataset => (
+                                    <TableRow onClick={() => {
+                                        alert("on click")
+                                    }}>
+                                        <TableCell>
+                                            {dataset.id}
+                                        </TableCell>
+                                        <TableCell>
+                                            <Badge variant="secondary">{dataset.format}</Badge>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Badge variant="outline">{dataset.license}</Badge>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
                             </TableBody>
                         </Table>
                     </CardContent>
