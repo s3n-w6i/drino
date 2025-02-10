a8e7f5739ef8019e26d6853726fdd4d7cb5be97f
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2025-02-10 12:01:43 +0100

    add gtfstidy as dependency for feed validation

--- shell.nix
+++ shell.nix
@@ -6,6 +6,20 @@ let
   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
   rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+  gtfstidy = pkgs.buildGoModule rec {
+    pname = "gtfstidy";
+    version = "0.2";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "patrickbr";
+      repo = "gtfstidy";
+      rev = "92a23c8dd16bdd2691468090c554f036c19df590";
+      sha256 = "sha256-xNO3KEMPLJ3ygeXYIs7J9AWq9xq9OMUKS0kUI72iIxE=";
+      # sha256 = "";
+    };
+
+    vendorHash = "sha256-WMpq9EP4ksxkgz1/R25piVgTB5TZe1DdzyGful7sixI=";
+  };
 in
 pkgs.mkShell {
   buildInputs = [
@@ -30,6 +44,7 @@ pkgs.mkShell {
         python-pkgs.scikit-learn
         python-pkgs.pyarrow
     ]))
+    gtfstidy
   ];
   RUST_BACKTRACE = 1;
 }
