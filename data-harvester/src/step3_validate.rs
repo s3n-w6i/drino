fbdcf4e46ac2efbdd30932491e4a88b8e22575e3
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2025-02-12 22:50:12 +0100

    improve error handling

--- data-harvester/src/step3_validate.rs
+++ data-harvester/src/step3_validate.rs
@@ -1,8 +1,8 @@
 use std::fmt::Display;
-use std::io::ErrorKind;
-use std::process::{Command, Stdio};
+use std::io::{BufRead, ErrorKind};
+use std::process::Command;
 use common::types::dataset::{DataSource, Dataset, DatasetFormat};
-use log::{error, warn};
+use log::{debug, error, warn};
 use crate::step2_import::{ImportStepExtra, ImportStepOutput};
 use crate::step3_validate::ValidateError::{GtfsTidy, UnknownFormat, UnsupportedDatasetSource};
 
@@ -10,7 +10,7 @@ pub async fn validate_data(
     ImportStepOutput { dataset, extra }: ImportStepOutput
 ) -> Result<ValidateStepOutput, ValidateError> {
     check_existence_of_tools()?;
-    
+
     let result = match dataset.format {
         DatasetFormat::Gtfs => {
             validate_gtfs(&dataset).await
@@ -41,10 +41,10 @@ fn check_existence_of_tools() -> Result<(), ValidateError> {
 
     if let Err(e) = gtfstidy_dummy_call {
         if let ErrorKind::NotFound = e.kind() {
-            return Err(GtfsTidy("GtfsTidy command was not found. Please install it or disable feed validation."));            
+            return Err(GtfsTidy("GtfsTidy command was not found. Please install it or disable feed validation.".into()));
         }
     }
-    
+
     Ok(())
 }
 
@@ -58,18 +58,34 @@ async fn validate_gtfs(dataset: &Dataset) -> Result<(), ValidateError> {
 
     let gtfstidy_out = Command::new("gtfstidy")
         .arg(filepath)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
         .output()?;
     
-    let status = gtfstidy_out.status.code()
-        .ok_or(GtfsTidy("Failed to receive status code from GtfsTidy"));
+    let status = gtfstidy_out.status.code();
 
     match status {
-        Ok(0) => Ok(()),
-        Err(e) => Err(e),
-        _ => Err(GtfsTidy("GtfsTidy failed"))
+        None => { return Err(GtfsTidy("Unable to get GtfsTidy's status code".into()))}
+        Some(0) => {}, // All good, continue printing normal output
+        Some(_) => {
+            // Collect gtfstidy's error output and put it into the error message
+            let error_message = gtfstidy_out.stderr.lines()
+                .map(|line| {
+                    line.unwrap_or_else(|e| format!("Unable to error output line: {e}"))
+                })
+                .collect::<Vec<String>>()
+                .join("\n");
+            return Err(GtfsTidy(error_message));
+        }
     }
+
+    // Forward standard output of gtfstidy
+    gtfstidy_out.stdout.lines().for_each(|line| {
+        match line {
+            Ok(line) => { debug!(target: "gtfstidy", "{}", line); }
+            Err(e) => { warn!(target: "gtfstidy", "{}", e); }
+        }
+    });
+
+    Ok(())
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -77,7 +93,7 @@ pub enum ValidateError {
     UnknownFormat,
     UnsupportedDatasetSource,
     IO(#[from] std::io::Error),
-    GtfsTidy(&'static str),
+    GtfsTidy(String),
 }
 
 impl Display for ValidateError {
