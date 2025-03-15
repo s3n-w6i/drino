5094565a876ab9d0f30c03a7696ed159228b4123
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2025-03-16 00:13:00 +0100

    change default file format to json

--- src/bootstrap_config.rs
+++ src/bootstrap_config.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[derive(Parser, Clone)]
 #[command(version, about)]
 pub struct BootstrapConfig {
-    #[clap(short('c'), long("config"), env("DRINO_CONFIG"), default_value_os = "config.yaml")]
+    #[clap(short('c'), long("config"), env("DRINO_CONFIG"), default_value_os = "config.json")]
     pub config_file: String,
     #[clap(short('l'), long("log-level"), env("DRINO_LOG_LEVEL"), default_value_t, value_enum)]
     pub log_level: LogLevel,
