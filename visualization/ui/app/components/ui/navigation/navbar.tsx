53fd248171dd57c70e77dc2ac88f43dc46e218cc
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2025-03-22 21:52:23 +0100

    WIP: new sidebar

--- visualization/ui/app/components/ui/navigation/navbar.tsx
+++ visualization/ui/app/components/ui/navigation/navbar.tsx
@@ -1,10 +1,16 @@
 import * as React from "react"
 
-import {Tooltip, TooltipContent, TooltipTrigger} from "~/components/ui/tooltip";
-
-import {Database, Home, Map, Route, Settings} from "lucide-react"
-import Drino from "~/components/ui/icon/drino";
+import {Database, Home, Map, Route} from "lucide-react"
 import {NavLink, useLocation} from "react-router";
+import {
+    Sidebar,
+    SidebarContent, SidebarFooter,
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem
+} from "~/components/ui/sidebar";
+import {Progress} from "~/components/ui/progress";
+import Drino from "~/components/ui/icon/drino";
 
 
 const NAV_BAR_ITEMS = [
@@ -30,57 +36,86 @@ const NAV_BAR_ITEMS = [
     }
 ]
 
-const NavBar = React.forwardRef<
-    HTMLDivElement,
-    React.HTMLAttributes<HTMLDivElement>
->(() => {
-        const location = useLocation();
+const NavBar = () => {
+    const location = useLocation();
 
-        return (
-            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-                <nav className="flex flex-col items-center gap-4 px-2 py-4">
-                    <NavLink
-                        to="/"
-                        className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-                    >
-                        <Drino className="h-4 w-4 transition-all group-hover:scale-110"/>
-                        <span className="sr-only">drino Dashboard</span>
-                    </NavLink>
-                    {NAV_BAR_ITEMS.map(({link, icon, title}) => (
-                        <Tooltip key={link}>
-                            <TooltipTrigger asChild>
-                                <NavLink
-                                    to={link}
-                                    className={
-                                        (link == location?.pathname) ? "flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                                            : "flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                                    }>
-                                    {React.cloneElement(icon, {className: "h-5 w-5"})}
-                                    <span className="sr-only">{title}</span>
-                                </NavLink>
-                            </TooltipTrigger>
-                            <TooltipContent side="right">{title}</TooltipContent>
-                        </Tooltip>
-                    ))}
-                </nav>
-                <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-4">
-                    <Tooltip>
+    return (
+        <Sidebar>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <NavLink to="/">
+                                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
+                                    <Drino className="size-4"/>
+                                </div>
+                                    <div className="grid flex-1 text-left text-sm leading-tight">
+                                        <span className="font-bold text-lg">Drino</span>
+                                    </div>
+                            </NavLink>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Home</SidebarGroupLabel>
+                </SidebarGroup>
+            </SidebarContent>
+
+            <SidebarFooter>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Status: Preprocessing</SidebarGroupLabel>
+                    <div className="m-2 flex flex-row items-baseline gap-4">
+                        <span className="font-semibold">1/5</span>
+                        <Progress value={22}/>
+                    </div>
+                </SidebarGroup>
+            </SidebarFooter>
+        </Sidebar>
+        /*<aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+            <nav className="flex flex-col items-center gap-4 px-2 py-4">
+                <NavLink
+                    to="/"
+                    className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+                >
+                    <Drino className="h-4 w-4 transition-all group-hover:scale-110"/>
+                    <span className="sr-only">drino Dashboard</span>
+                </NavLink>
+                {NAV_BAR_ITEMS.map(({link, icon, title}) => (
+                    <Tooltip key={link}>
                         <TooltipTrigger asChild>
                             <NavLink
-                                to="settings"
-                                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                            >
-                                <Settings className="h-5 w-5"/>
-                                <span className="sr-only">Settings</span>
+                                to={link}
+                                className={
+                                    (link == location?.pathname) ? "flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                                        : "flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                                }>
+                                {React.cloneElement(icon, {className: "h-5 w-5"})}
+                                <span className="sr-only">{title}</span>
                             </NavLink>
                         </TooltipTrigger>
-                        <TooltipContent side="right">Settings</TooltipContent>
+                        <TooltipContent side="right">{title}</TooltipContent>
                     </Tooltip>
-                </nav>
-            </aside>
-        )
-    }
-)
+                ))}
+            </nav>
+            <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-4">
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <NavLink
+                            to="settings"
+                            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                        >
+                            <Settings className="h-5 w-5"/>
+                            <span className="sr-only">Settings</span>
+                        </NavLink>
+                    </TooltipTrigger>
+                    <TooltipContent side="right">Settings</TooltipContent>
+                </Tooltip>
+            </nav>
+        </aside>*/
+    )
+}
 
 NavBar.displayName = "NavBar"
 
--- visualization/ui/app/layouts/sidebar.tsx
+++ visualization/ui/app/layouts/sidebar.tsx
@@ -2,18 +2,30 @@ import {NavBar} from "~/components/ui/navigation/navbar";
 import ReactRouterBreadcrumbs from "~/components/ui/navigation/react-router-breadcrumbs";
 import {Outlet} from "react-router";
 import * as React from "react";
+import {SidebarInset, SidebarProvider, SidebarTrigger} from "~/components/ui/sidebar";
+import {Separator} from "~/components/ui/separator";
 
+// noinspection JSUnusedGlobalSymbols
 export default function SidebarLayout() {
+    const [sidebarOpen, setSidebarOpen] = React.useState(true);
+
     return (
-        <div className="flex min-h-screen w-full flex-col">
+        <SidebarProvider
+            open={sidebarOpen}
+            onOpenChange={(value) => setSidebarOpen(value)}
+            defaultOpen={true}>
             <NavBar/>
-            <div className="flex h-screen flex-col sm:pt-4 sm:pl-14">
-                <main className="flex-column flex-1 items-_start">
-                    <div className="p-4 sm:p-6 md:gap-8">
-                        <ReactRouterBreadcrumbs/>
-                    </div>
+            <SidebarInset className="flex-col">
+                <header className="p-4 sm:p-6 flex flex-row items-center gap-2">
+                    <SidebarTrigger/>
+                    <Separator orientation="vertical" className="mr-2 h-4"/>
+                    <ReactRouterBreadcrumbs/>
+                </header>
+                <Separator />
+                <div className="grow flex flex-col sm:pt-2 bg-muted/80">
                     <Outlet/>
-                </main>
+                </div>
+                <Separator />
                 <footer className="bg-background py-4 md:px-8 md:py-0">
                     <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
                         <p className="text-xs text-muted-foreground leading-loose">
@@ -21,7 +33,7 @@ export default function SidebarLayout() {
                         </p>
                     </div>
                 </footer>
-            </div>
-        </div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
